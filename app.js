307a0daa079a3f958360eeb2eeff86f3ecb2485c
Shippu002 <adelajaalameen@gmail.com>
2025-01-21 07:51:20 -0800

    set up express server with routes for home, about, and contact

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
-console.log("It all start from localhost:3000")
-console.log("Hello API")
-console.log("Hello")
+import express from "express";
+
+// initialize express
+const app = express();
+const PORT = 3000;
+
+// define a route
+app.get("/", (req, res) => {
+  res.send("Hello Backend development with express")
+})
+
+
+app.get("/about", (req, res) => {
+  res.send("<h2>This is about us page</h2>");
+});
+
+app.get("/contact", (req, res) => {
+  res.send("<h2>This is contact page</h2>");
+});
+
+
+// start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`)
+})
